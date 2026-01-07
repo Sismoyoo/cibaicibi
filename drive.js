8d40b67bdd85e3a0393e36c4cb7349655fa7e821
Sismoyoo <sismoyoo@gmail.com>
2026-01-08 00:21:48 +0700

    Update drive.js

--- drive.js
+++ drive.js
@@ -1,45 +1,41 @@
-const CLIENT_ID = "ISI_CLIENT_ID";
-const SCOPES = "https://www.googleapis.com/auth/drive.file";
-let accessToken = null;
+const CLIENT_ID="ISI_CLIENT_ID_KAMU"
+const SCOPE="https://www.googleapis.com/auth/drive.file"
+let token=null
 
 function loginGoogle(){
-  google.accounts.oauth2.initTokenClient({
-    client_id: CLIENT_ID,
-    scope: SCOPES,
-    callback: t=>{
-      accessToken=t.access_token;
-      statusBackup.innerText="Login berhasil";
-    }
-  }).requestAccessToken();
+google.accounts.oauth2.initTokenClient({
+client_id:CLIENT_ID,
+scope:SCOPE,
+callback:t=>{
+token=t.access_token
+statusBackup.innerText="Login berhasil"
+}
+}).requestAccessToken()
 }
 
 function backupKeDrive(){
-  if(!accessToken)return alert("Login Google dulu");
-
-  const data={};
-  db.transaction("menu").objectStore("menu").getAll().onsuccess=e=>{
-    data.menu=e.target.result;
-    db.transaction("transaksi").objectStore("transaksi").getAll().onsuccess=p=>{
-      data.transaksi=p.target.result;
-      upload(data);
-    };
-  };
+if(!token)return alert("Login Google dulu")
+const data={}
+db.transaction("menu").objectStore("menu").getAll().onsuccess=e=>{
+data.menu=e.target.result
+db.transaction("transaksi").objectStore("transaksi").getAll().onsuccess=t=>{
+data.transaksi=t.target.result
+upload(data)
+}
+}
 }
 
 function upload(data){
-  const blob=new Blob([JSON.stringify(data)],{type:"application/json"});
-  const meta={name:`cibaicibi-${Date.now()}.json`};
-  const form=new FormData();
-  form.append("metadata",new Blob([JSON.stringify(meta)],{type:"application/json"}));
-  form.append("file",blob);
-
-  fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",{
-    method:"POST",
-    headers:{Authorization:`Bearer ${accessToken}`},
-    body:form
-  }).then(()=>statusBackup.innerText="Backup sukses");
+const f=new FormData()
+f.append("metadata",new Blob([JSON.stringify({name:`cibaicibi-${Date.now()}.json`})],{type:"application/json"}))
+f.append("file",new Blob([JSON.stringify(data)],{type:"application/json"}))
+fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",{
+method:"POST",
+headers:{Authorization:`Bearer ${token}`},
+body:f
+}).then(()=>statusBackup.innerText="Backup sukses")
 }
 
 function autoBackup(){
-  if(navigator.onLine && accessToken) backupKeDrive();
+if(navigator.onLine&&token)backupKeDrive()
 }
