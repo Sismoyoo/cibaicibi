46f5e0e6613a83cb666bafd037fc1a548fcf300c
Sismoyoo <sismoyoo@gmail.com>
2026-01-08 04:50:38 +0700

    Fix syntax error in service worker install event

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
   e.waitUntil(
     caches.open("cibaicibi").then(c=>c.addAll([
-      "./","./index.html","./app.js","./drive.js"
+      "./","./index.html","./app.js"
     ]))
-  );
-});
+  )
+})
